489e1493826982390319aebc566373c72dc3b311
Glitch (secretive-fuzzy-moth) <none>
2020-10-30 01:08:35 +0000

    💓📮 Updated with Glitch

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+$(".hideButton").click(function(){
+  $(".ghost-image").hide();
+});
 
+$(".showButton").click(function(){
+  $(".ghost-image").show();
+});
+
+$(".updateButton").click(function(){
+  $("img").attr("src", "https://media0.giphy.com/media/l4pMdRYrYUKI6I2ZO/giphy.gif");
+  $("body").css("background-color", "black");
+  $("body").css("color", "white");
+});
+
+$(".msgButton").click(function(){
+  $(".message").text("boo?");
+});
+
+$(".nameButton").click(function(){
+  let newName = $(".input").val();
+  if (newName === "Bob"){
+    $("h1").text("He he! Yes my name is "+ newName+"!")
+  }
+  if (newName != "Bob"){
+    $("h1").text("No no noo!! My name isn't "+ newName +"!")
+  }
+});
